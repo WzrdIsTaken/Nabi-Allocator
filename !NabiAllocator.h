bae2d45a557a6a5b0338ca409f79c4d23f5d98f3
WzrdIsTaken <62243693+WzrdIsTaken@users.noreply.github.com>
2024-01-15 13:07:41 +0000

    Commented out some defines which probs aren't useful for 'drag, drop and go'
    + removed a rogue #

--- !NabiAllocator.h
+++ !NabiAllocator.h
@@ -27,13 +27,13 @@
 #define NA_THREAD_SAFE_HEAP_ZONE // Adds a mutex/lock in HeapZone::Allocate and Free (default std::malloc/std::free are already thread safe)
 
 // Allocator
-#define NA_TRACK_ALLOCATIONS // Memory allocators will track the count and total size of active and all time allocated objects 
-#define NA_MEMORY_TAGGING // Adds metadata to all blocks. Note: this increases the size of each blocks by sizeof(nabi_allocator::memoryTag (in MemoryConstants.h))
+//#define NA_TRACK_ALLOCATIONS // Memory allocators will track the count and total size of active and all time allocated objects 
+//#define NA_MEMORY_TAGGING // Adds metadata to all blocks. Note: this increases the size of each blocks by sizeof(nabi_allocator::memoryTag (in MemoryConstants.h))
 
 // Tests
-#define NA_TESTS // Run unit tests
-#define NA_BENCHMARKS // Run benchmark tests
-#define NA_WORKFLOWS // Run workflow tests
+//#define NA_TESTS // Run unit tests
+//#define NA_BENCHMARKS // Run benchmark tests
+//#define NA_WORKFLOWS // Run workflow tests
 
 #define NA_BENCHMARK_RUN_COUNT 3u // If NA_BENCHMARKS is defined, how many times to run each benchmark
 
@@ -2930,7 +2930,7 @@ namespace nabi_allocator
 		blockHeader = NA_REINTERPRET_MEMORY_DEFAULT(BlockHeader, blockHeaderPosition);
 		UnloadBlockInfo(blockInfoContent, *blockHeader);
 
-		// Store the block's infomation in allocatorBlockInfo#
+		// Store the block's infomation in allocatorBlockInfo
 		allocatorBlockInfo.m_MemoryAddress = NA_TO_UPTR(blockHeader);
 #ifdef NA_MEMORY_TAGGING
 		allocatorBlockInfo.m_MemoryTag = blockInfoContent.m_MemoryTag;
